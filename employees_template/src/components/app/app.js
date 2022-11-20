7aed9f64defced4ebed641b43f30f925414f3f80
TokiOZZ <trotsenkop@gmail.com>
2022-11-20 23:50:33 +0200

    Point before working with increase propertyu

--- employees_template/src/components/app/app.js
+++ employees_template/src/components/app/app.js
@@ -7,19 +7,24 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
-  return (
-    <div className="app">
-        <AppInfo />
+    const data = [
+        { name: 'John C.', salary: 800 },
+        { name: 'Alex M..', salary: 3000 },
+        { name: 'Carl W..', salary: 5000 },
+    ]
+    return (
+        <div className="app">
+            <AppInfo />
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
+            <div className="search-panel">
+                <SearchPanel />
+                <AppFilter />
+            </div>
+
+            <EmployeesList data={data} />
+            <EmployeesAddForm />
         </div>
-        
-        <EmployeesList/>
-        <EmployeesAddForm/>
-    </div>
-  );
+    );
 }
 
 export default App;
